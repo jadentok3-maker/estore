b6c3f5e743f6ea772b3069d108c63db4a7874e51
Yong Hao <yonghaotan09@gmail.com>
2025-11-17 13:42:32 +0800

    Update dbinfo.php

--- dbinfo.php
+++ dbinfo.php
@@ -3,4 +3,11 @@ $hostname = "localhost";
 $dbUser = "root";
 $dbPassword = "";
 $db = "estore";
+
+$mysqli = new mysqli($hostname,$dbUser,$dbPassword,$db); 
+
+if ($mysqli -> connect_errno) {
+echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+exit();
+}
 ?>
