d6b60244ac014b21034fe5f03bbe27d89535c513
m-attr <1007mattansel@gmail.com>
2025-11-17 13:58:09 +0800

    Update display.php

--- display.php
+++ display.php
@@ -1,8 +1,8 @@
 <?php
-    require_once("dbinfo.php");
+    $database = require_once("dbinfo.php");
 
-    // $sql = ;
-    // $result = $conn->query($sql);
+    $sql = "SELECT name, price, qty FROM estore";
+    $result = $conn->query($sql);
 
     echo "<!DOCTYPE html>
     <html lang='en'>
@@ -11,5 +11,38 @@
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
             <title>Product Display</title>
         </head>
+
+        <body>
+            <h1>All Products</h1>";
+
+            if ($result->num_rows > 0) 
+            {
+                echo "<table>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Description</th>
+                        </tr>";
+            
+                while ($row = $result->fetch_assoc()) 
+                {
+                    echo "<tr>
+                            <td>" . $row["id"] . "</td>
+                            <td>" . $row["name"] . "</td>
+                            <td>$" . $row["price"] . "</td>
+                            <td>" . $row["description"] . "</td>
+                        </tr>";
+                }
+                    echo "</table>";
+            } 
+            else 
+            {
+                echo "No products found.";
+            }
+        
+    $conn->close();
+
+    echo "</body>
     </html>";
-?>
+?>  
