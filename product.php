1029d961e230ac67b960afdeda9623e309bc4030
jadentok3-maker <jadentok3@gmail.com>
2025-11-17 13:57:14 +0800

    Update product.php

--- product.php
+++ product.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";       
 $password = "";           
-$dbname = "";
+$dbname = "estore";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
