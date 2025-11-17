7d9b02101a1496b0cdecc9b421e034ed0b250b83
jadentok3-maker <jadentok3@gmail.com>
2025-11-17 13:55:16 +0800

    Update product.php

---
+++ product.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";       
+$password = "";           
+$dbname = "";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+
+if (isset($_POST['add'])) {
+
+    $name = $_POST['name'];
+    $price = $_POST['price'];
+    $qty = $_POST['qty'];
+
+    $sql = "INSERT INTO products (name, price, qty)
+            VALUES ('$name', '$price', '$qty')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Product added successfully!";
+        header("");
+        exit();
+    } else {
+        echo "Error: " . mysqli_error($conn);
+    }
+
+}
+
